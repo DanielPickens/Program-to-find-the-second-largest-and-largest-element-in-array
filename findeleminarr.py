39c84b29611218dc58d73b8b478d648f61e0eab6
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-09-27 15:35:03 -0600

    Create findeleminarr.py

---
+++ findeleminarr.py
@@ -0,0 +1,13 @@
+
+list1 = [1, 2, 4, 5, 6, 7]
+mx=max(list1[0], list1[1])
+n =len(list1)
+for i in range(2,n):
+    if list1[i]>mx:
+        secondmax=mx  ## this locates or points to the second highest value element
+        mx=list1[i] #points ot largest element in arr
+    elif list1[i]>secondmax and \
+        mx != list1[i]:
+        secondmax=list1[i]
+print("second highest number is : ",\
+      str(secondmax))
