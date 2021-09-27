4e11056a281c7cbe8fe1c927bb95a4ec17da8c60
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-09-27 16:05:08 -0600

    Update findeleminarr.py

--- findeleminarr.py
+++ findeleminarr.py
@@ -1,4 +1,4 @@
-
+##finds second largest element in an array
 list1 = [1, 2, 4, 5, 6, 7]
 mx=max(list1[0], list1[1])
 n =len(list1)
@@ -11,3 +11,19 @@ for i in range(2,n):
         secondmax=list1[i]
 print("second highest number is : ",\
       str(secondmax))
+
+
+
+#finds largest element in array
+list1 = [1, 2, 4, 5, 6, 7]
+mx=max(list1[0], list1[1])
+n =len(list1)
+for i in range(2,n):
+    if list1[i]>mx:
+        secondmax=mx  ## this locates or points to the second highest value element
+        mx=list1[i] #points to largest element in arr
+    elif list1[i]>secondmax and \
+        mx != list1[i]:
+        secondmax=list1[i]
+print("second highest number is : ",\
+      str(mx))
